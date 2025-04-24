3e0477e036e0ad6d59895b5f0b1deb0969d3ee6e
irfan12ahmed <155778748+irfan12ahmed@users.noreply.github.com>
2025-04-24 14:47:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to Docker with Port 5003!"
+    return "Hello!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8083)
